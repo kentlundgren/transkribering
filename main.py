aefd7fd881286d96dbeb7a1c02312279363bc7d9
kentlundgren <131652205+kentlundgren@users.noreply.github.com>
2023-04-28 12:25:54 +0200

    Update main.py
    Bakgrund

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import os
 import subprocess
 
+# efter att blivit peppad av podden "Teknik i akademi" https://poddtoppen.se/podcast/1661532745/teknik-i-akademi
+# gjordes detta försök att göra ett program som använder sig av OpenAI:s Whisper för att transkribera från ljud till text
+         # https://openai.com/research/whisper
+# se bakgrunden i konversation med ChatGPT https://chat.openai.com/c/07a229c1-c7ba-4a78-8acf-51c5d4fcf8fe 
+# samt under 1 min "genomgång" https://drive.google.com/file/d/1NpZ6hMHPPeeBSdBNIcUOaeEH6D_hj182/view 
+# ett (av flera...;>) problem med programmet nedan - är att den inte hittar ffmpeg
+
 # Install Whisper package using pip
 subprocess.run(["pip", "install", "--upgrade", "--no-deps", "--force-reinstall", "git+https://github.com/openai/whisper.git"])
 
