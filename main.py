d029fa5d7db00f3e7c64f6e1825bdf7f0115eb88
kentlundgren <131652205+kentlundgren@users.noreply.github.com>
2023-04-28 14:32:06 +0200

    Går nu via API
    Går nu via API

--- main.py
+++ main.py
@@ -1,28 +1,51 @@
-import os
+ain3.py
+import requests
 import subprocess
+import openai
 
 # efter att blivit peppad av podden "Teknik i akademi" https://poddtoppen.se/podcast/1661532745/teknik-i-akademi
 # gjordes detta försök att göra ett program som använder sig av OpenAI:s Whisper för att transkribera från ljud till text
          # https://openai.com/research/whisper
-# se bakgrunden i konversation med ChatGPT https://chat.openai.com/c/07a229c1-c7ba-4a78-8acf-51c5d4fcf8fe 
-# samt under 1 min "genomgång" https://drive.google.com/file/d/1NpZ6hMHPPeeBSdBNIcUOaeEH6D_hj182/view 
-# ett (av flera...;>) problem med programmet nedan - är att den inte hittar ffmpeg
+# Detta är tredje försöket, med ett program som går via OpenAI:s API
+# MEN ett (av flera...;>) problem med programmet är fortfarande problem att den inte hittar ffmpeg på rad 29
 
-# Install Whisper package using pip
-subprocess.run(["pip", "install", "--upgrade", "--no-deps", "--force-reinstall", "git+https://github.com/openai/whisper.git"])
+# Användarens sökväg till ljudfilen på webben
+audio_path = input("Ange sökväg till ljudfilen på webben: ")
 
-# Prompt user to enter path to the audio file
-audio_path = input("Enter path to the audio file: ")
+# Hämta ljudfilen från webben
+try:
+    response = requests.get(audio_path)
+    response.raise_for_status()
+    with open("audio.mp3", "wb") as f:
+        f.write(response.content)
+    print("Ljudfilen har hämtats.")
+except requests.exceptions.HTTPError as e:
+    print(f"HTTP-fel: {e}")
+except requests.exceptions.RequestException as e:
+    print(f"Fel vid hämtning av ljudfil: {e}")
+    exit()
 
-# Convert audio file to wav format using ffmpeg
-output_path = os.path.splitext(audio_path)[0] + ".wav"
-subprocess.run(["ffmpeg", "-i", audio_path, output_path])
+# Konvertera ljudfilen till WAV-format
+try:
+    subprocess.run(["ffmpeg", "-i", "audio.mp3", "audio.wav"])
+    print("Ljudfilen har konverterats till WAV-format.")
+except subprocess.CalledProcessError as e:
+    print(f"Fel vid konvertering av ljudfil till WAV-format: {e}")
+    exit()
 
-# Transcribe the audio using Whisper
-with open(output_path, "rb") as f:
-    audio_bytes = f.read()
-
-response = subprocess.run(["whisper", "transcribe"], input=audio_bytes, capture_output=True)
-
-# Print the transcription result
-print(response.stdout.decode("utf-8"))
+# Transkribera ljudfilen med Whispers API
+try:
+    response = requests.post(
+        "https://api.whisper.ai/v1/transcriptions",
+        headers={"Authorization": "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"},
+        json={"config": {"languageCode": "en-US", "audioChannelCount": 1}},
+        files={"audio": ("audio.wav", open("audio.wav", "rb"), "audio/wav")},
+    )
+    response.raise_for_status()
+    print("Transkribering klar.")
+    print(response.json()["text"])
+except requests.exceptions.HTTPError as e:
+    print(f"HTTP-fel: {e}")
+except requests.exceptions.RequestException as e:
+    print(f"Fel vid anrop till Whispers API: {e}")
+    exit()
